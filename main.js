17b47c966ad145b527676fe90562207570d5b997
iamvickypedia <mailvikastiwari@gmail.com>
2022-09-25 21:08:49 +0530

    greeting done

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const clockContainer = document.querySelector(".clock-container");
 const dateContainer = document.querySelector(".date-container");
+const messageContainer = document.querySelector(".message-container");
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-input");
 
@@ -11,6 +12,15 @@ const initialize = () => {
     .split(" ")
     .slice(0, 3)
     .join(" ");
+  let greetingMessage = "";
+  if(currentTimeObj.getHours() > 4 && currentTimeObj.getHours() < 12) {
+    greetingMessage = "Good morning"
+  }else if(currentTimeObj.getHours() > 12 && currentTimeObj.getHours() < 22) {
+    greetingMessage = "Good evening"
+  }else{
+    greetingMessage = "Good night"
+  }
+  messageContainer.innerHTML = greetingMessage;
   searchInput.value = "";
   searchInput.focus();
 
